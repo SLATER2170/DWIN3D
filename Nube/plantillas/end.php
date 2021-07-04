27a240dcff8d1a70999e51a9d63b5ad4022d56b7
SLATER2170 <71773912+SLATER2170@users.noreply.github.com>
2021-07-04 13:09:22 +1000

    Initialize dependabot/npm_and_yarn/Nube/Micv/hosted-git-info-2.8.9

---
+++ Nube/plantillas/end.php
@@ -0,0 +1,6 @@
+<section class="py-5">
+  <div class="container">
+    <h2 class="font-weight-light">Consultas de padron</h2>
+    <p>Pagina para consultar el padron Dominicano, solo necesita poner tu cedula y te arrojara los datos de la persona que este vinculada a la cedula, ANIMATE!</p>
+  </div>
+</section>
